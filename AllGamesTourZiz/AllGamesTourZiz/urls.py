8602c4f50a4f70a55ad9590660ea5a997f9dbf0e
ZizTo <pashamatievski@gmail.com>
2026-06-12 14:15:51 +0300

    static goddamn added

--- AllGamesTourZiz/AllGamesTourZiz/urls.py
+++ AllGamesTourZiz/AllGamesTourZiz/urls.py
@@ -27,8 +27,6 @@ urlpatterns = [
     path('games/', include('games.urls')),
     path('sessions/', include('playSessions.urls')),
     path('admin/', admin.site.urls),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
 
-#if settings.DEBUG:
-#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
